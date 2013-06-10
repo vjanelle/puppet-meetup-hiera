a38baae8f9a120acc2349623a0ff82c081ba2d55
Vincent Janelle <randomfrequency@gmail.com>
2013-06-09 23:15:52 -0700

    start tinkering with puppet's config file

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,12 +1,36 @@
-package {'cifs-utils':
-  ensure => 'installed'
+include stdlib
+
+class requirements {
+  package {'cifs-utils':
+    ensure => 'installed'
+  }
+
+  package { 'vim-enhanced':
+    ensure => 'installed',
+  }
+
+  package { 'puppet-server':
+    ensure => installed,
+  }
 }
 
-package { 'vim-enhanced':
-  ensure => 'installed',
+class { 'requirements':
+  stage => 'setup'
 }
 
-package { 'puppet-server':
-  ensure => installed,
+Ini_setting {
+  ensure  => present,
+  path    => '/etc/puppet/puppet.conf',
+  section => 'master',
+}
+
+ini_setting { 'change master hieraconf':
+  setting => 'hiera_config',
+  value   => '/vagrant/hierademo/hiera.yaml',
+}
+
+ini_setting { 'change master modulepath':
+  setting => 'modulepath',
+  value   => '/vagrant/modules:/etc/puppet/modules:/usr/share/puppet/modules'
 }
 
